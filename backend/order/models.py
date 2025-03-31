33dfc5948e3ccee164d4ddbe4331a5f2b59c9732
Ethan <ethanchang34@yahoo.com>
2025-03-31 19:23:14 -0400

    added Literals to enforce type

--- backend/order/models.py
+++ backend/order/models.py
@@ -1,32 +1,40 @@
-from typing import List, Optional, Union
+from typing import List, Optional, Literal
 from pydantic import BaseModel
 
+ProteinType = Literal["Chicken", "Steak", "Barbacoa", "Carnitas", "Sofritas", "Veggie"]
+RiceType = Literal["White", "Brown", "None"]
+BeanType = Literal["Blank", "Pinto", "None"]
+ToppingType = Literal["Lettuce", "Fajita Veggies", "Mild Salsa", "Medium Salsa", "Hot Salsa", "Corn Salsa", "Cheese", "Sour Cream", "Guacamole"]
+SideType = Literal["Chips", "Chips & Guac", "Chips & Queso"]
+DrinkType = Literal["Water", "Lemonade", "Soda"]
+Size = Literal["Small", "Medium", "Large"]
+
 class Topping(BaseModel):
-    name: str  # e.g., "Cheese", "Sour Cream"
+    name: ToppingType
     quantity: Optional[int] = 1  # How many scoops/servings of this topping
 
 class SideItem(BaseModel):
-    name: str  # e.g., "Chips", "Chips & Guac"
+    name: SideType
     quantity: Optional[int] = 1
 
 class Drink(BaseModel):
-    name: str  # e.g., "Coke", "Lemonade"
-    size: Optional[str] = "Medium"
+    name: DrinkType
+    size: Optional[Size] = "Medium"
     quantity: Optional[int] = 1
 
 class OrderItem(BaseModel):
     id: str
     type: str  # "Bowl", "Burrito", etc.
-    protein: str
+    protein: ProteinType
     protein_quantity: Optional[int] = 1 
-    rice: Optional[str] = None
+    rice: Optional[RiceType] = None
     rice_quantity: Optional[int] = 1
-    beans: Optional[str] = None
+    beans: Optional[BeanType] = None
     beans_quantity: Optional[int] = 1
-    toppings: Optional[List[Topping]] = []
-    sides: Optional[List[SideItem]] = []
-    drinks: Optional[List[Drink]] = []
-    quantity: int  # quantity of the entree item
+    toppings: Optional[List[ToppingType]] = []
+    sides: Optional[List[SideType]] = []
+    drinks: Optional[List[DrinkType]] = []
+    quantity: int = 1
 
 class Cart(BaseModel):
     items: List[OrderItem]
