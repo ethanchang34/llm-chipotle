86ce64b632dce2ae53061e81421bc259d75d1818
Ethan <ethanchang34@yahoo.com>
2025-03-31 19:46:46 -0400

    update model

--- backend/order/models.py
+++ backend/order/models.py
@@ -2,36 +2,43 @@ from typing import List, Optional, Literal
 from pydantic import BaseModel
 
 OrderType = Literal["Bowl", "Burrito", "Tacos", "Salad", "Quesadilla"]
-ProteinType = Literal["Chicken", "Steak", "Barbacoa", "Carnitas", "Sofritas", "Veggie"]
-RiceType = Literal["White", "Brown", "None"]
-BeanType = Literal["Blank", "Pinto", "None"]
+
+class Protein(BaseModel):
+    name: Literal["Chicken", "Steak", "Barbacoa", "Carnitas", "Sofritas", "Veggie"]
+    quantity: int = 1
+
+class Rice(BaseModel):
+    name: Literal["White", "Brown", "None"]
+    quantity: int = 1
+
+class Bean(BaseModel):
+    name: Literal["Blank", "Pinto", "None"]
+    quantity = int = 1
 
 class Topping(BaseModel):
     name: Literal["Lettuce", "Fajita Veggies", "Mild Salsa", "Medium Salsa", "Hot Salsa", "Corn Salsa", "Cheese", "Sour Cream", "Guacamole"]
-    quantity: Optional[int] = 1
+    quantity: int = 1
 
-class SideItem(BaseModel):
+class Side(BaseModel):
     name: Literal["Chips", "Chips & Guac", "Chips & Queso"]
-    quantity: Optional[int] = 1
+    quantity: int = 1
 
 class Drink(BaseModel):
     name: Literal["Water", "Lemonade", "Soda"]
-    size: Literal["Small", "Medium", "Large"]
-    quantity: Optional[int] = 1
+    size: Literal["Small", "Medium", "Large"] = "Medium"
+    quantity: int = 1
 
-class OrderItem(BaseModel):
+class Entree(BaseModel):
     id: str
     type: OrderType
-    protein: ProteinType
-    protein_quantity: Optional[int] = 1 
-    rice: Optional[RiceType] = None
-    rice_quantity: Optional[int] = 1
-    beans: Optional[BeanType] = None
-    beans_quantity: Optional[int] = 1
+    protein: List[Protein] = []
+    rice: Optional[List[Rice]] = []
+    beans: Optional[List[Bean]] = []
     toppings: Optional[List[Topping]] = []
-    sides: Optional[List[SideItem]] = []
-    drinks: Optional[List[Drink]] = []
     quantity: int = 1
 
 class Cart(BaseModel):
-    items: List[OrderItem]
+    entree: List[Entree]
+    sides: Optional[List[Side]] = []
+    drinks: Optional[List[Drink]] = []
+
